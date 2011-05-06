0693ba94808415e5308deacab19ac1a2d2a9e862
scottweisman <sweisman18@gmail.com>
2011-05-05 23:21:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtconnect::Application.config.secret_token = '7d69b0bf7b318ad73a05fc88f7e0e8f25a30ec284749c07e77c6ef3d461d030e4f02a5b9db56456c76f4d0945fe0cb4685b55229a97c4a94750f84f095d80cd3'
