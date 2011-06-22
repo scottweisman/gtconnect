6ff4428a8e9bda97a62ef9a7af08ba9940b646a2
scottweisman <sweisman18@gmail.com>
2011-06-21 22:38:59 -0500

    added jquery toggle

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function() {  
+	
 	$("#marketing_contact_last_contact").datepicker({dateFormat: 'mm-dd-yy'});  
+	
+	$(".hidden_info").hide();
+	$("h2.contact_link").click(function () {
+		$(this).toggleClass("active").next().slideToggle("fast");
+		return false;		
+	});
 });
