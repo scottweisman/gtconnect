6862a808f2291af8d5fb99c6eb0ef7eb6efd7834
scottweisman <sweisman18@gmail.com>
2011-06-23 23:05:42 -0500

    added simpleform and jquery for new contact fields

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ gem "jquery-rails"
 gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
 
 gem 'css3buttons'
+gem 'simple_form'
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,4 +9,29 @@ $(function() {
 		$(this).toggleClass("active").next().slideToggle("fast");
 		return false;		
 	});
+	
+	$('.website_opt_input').hide();
+	$('p.website_opt_text').click(function () {
+		$('p.website_opt_text').hide();
+		$('.website_opt_input').show("slow");
+	});
+	
+	$('.facebook_opt_input').hide();
+	$('p.facebook_opt_text').click(function () {
+		$('p.facebook_opt_text').hide();
+		$('.facebook_opt_input').show("slow");
+	});
+	
+	$('.twitter_opt_input').hide();
+	$('p.twitter_opt_text').click(function () {
+		$('p.twitter_opt_text').hide();
+		$('.twitter_opt_input').show("slow");
+	});
+	
+	$('.address_opt_input').hide();
+	$('p.address_opt_text').click(function () {
+		$('p.address_opt_text').hide();
+		$('.address_opt_input').show("slow");
+	});
+	
 });
